1ee60c5ba35aee330da30d7dd3b5214ff737e3ca
Brandonjca <brandito144@gmail.com>
2023-01-14 17:30:49 -0500

    footer, factura

---
+++ CafeOhLaLa/tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js,ts}"],
+  theme: {
+    extend: {},
+    colors: {
+      blanco: "#ffffff",
+      chocolate: "#543217",
+      crema: "#c0904b",
+      chocolate2: "https://fondosmil.com/fondo/15481.jpg"
+    }
+  },
+  plugins: [],
+}
