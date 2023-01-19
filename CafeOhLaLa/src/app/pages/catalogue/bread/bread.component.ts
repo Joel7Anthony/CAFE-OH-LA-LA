d0c65819076f0d858c75dfc5393c222dae930474
Sebastiamg <sebasortiz112004@gmail.com>
2023-01-18 22:05:57 -0500

    corrección panes by ME

--- CafeOhLaLa/src/app/pages/catalogue/bread/bread.component.ts
+++ CafeOhLaLa/src/app/pages/catalogue/bread/bread.component.ts
@@ -3,7 +3,7 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-bread',
   templateUrl: './bread.component.html',
-  styleUrls: ['./bread.component.css'],
+  styleUrls: ['./bread.component.css']
 })
 export class BreadComponent {
 
